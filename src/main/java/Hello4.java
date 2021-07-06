362c15146138b4c6bb4f2d364d6b381274834054
Jungmin-Seo0527 <soato1405@naver.com>
2021-07-07 00:40:29 +0900

    Update Hello4.java

--- src/main/java/Hello4.java
+++ src/main/java/Hello4.java
@@ -2,5 +2,6 @@ public class Hello4 {
     public void show() {
         System.out.println("Hello 4");
         System.out.println("Hello 444");
+        System.out.println("Hello 44444");
     }
 }
