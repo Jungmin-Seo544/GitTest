d6a1663fc10b849d7fe92854f35a3fc8a17adce6
Jungmin-Seo0527 <soato1405@naver.com>
2021-07-15 21:06:19 +0900

    디테일 페이지 추가

--- src/main/java/DetailPage.java
+++ src/main/java/DetailPage.java
@@ -2,5 +2,6 @@ public class DetailPage {
     public static void main(String[] args) {
         System.out.println("새 브랜치 생성 - feature/detail-page");
         System.out.println("feature/detail-page 브랜치에서 한번 더 커밋하기");
+        System.out.println("디테일 페이지");
     }
 }
