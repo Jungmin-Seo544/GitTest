08025145129ef443e46c5636875b120c5ace6cfb
Jungmin-Seo0527 <soato1405@naver.com>
2021-07-15 21:13:29 +0900

    장바구니 담기 기능

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -1,5 +1,6 @@
 public class Cart {
     public static void main(String[] args) {
         System.out.println("장바구니 담기");
+        System.out.println("장바구니 담기2");
     }
 }
--- src/main/java/Hello1.java
+++ src/main/java/Hello1.java
@@ -1,5 +1,6 @@
 public class Hello1 {
     public static void main(String[] args) {
         System.out.println("Hello1");
+        System.out.println("장바구니 담기");
     }
 }
